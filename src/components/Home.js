633a83262b3c152fc047d6be50fac5c394bc1896
jafar bino <binocoder@gmail.com>
2023-11-18 22:56:32 +0300

    Completed assignment

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,16 @@
-import React from "react";
-import { name, city } from "../data/data.js";
+import React from 'react';
+import { name, city } from "../data/data";
 
 function Home() {
-  // update the JSX being returned!
-  return <div>Home</div>;
+  const textStyle = {
+    color: 'firebrick'
+  };
+
+  return (
+    <div id="home">
+      <h1 style={textStyle}>{`${name} is a Web Developer from ${city}`}</h1>
+    </div>
+  );
 }
 
 export default Home;
